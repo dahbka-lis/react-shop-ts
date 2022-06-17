4f04613a118aea807fd5915490771392d1036979
DaHbka <flexinzflexinz@gmail.com>
2022-06-17 17:51:51 +0200

    +Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,51 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+import { Container } from './globalStyled';
+
+const FooterOuter = styled.div`
+    color: #fff;
+    background-color: var(--bg-footer);
+    padding-block: 1.5rem;
+`;
+
+const FooterInner = styled.div`
+    font-size: 0.8em;
+    letter-spacing: 1px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @media (max-width: 1024px) {
+        font-size: 0.5em;
+    }
+
+    & a {
+        color: #10b981;
+
+        &:hover {
+            color: #1cca90;
+        }
+    }
+`;
+
+interface IFooterProps {}
+
+const Footer: FC<IFooterProps> = () => {
+    return (
+        <FooterOuter>
+            <Container>
+                <FooterInner>
+                    <span>{new Date().getFullYear()} Gracechapel</span>
+                    <span>
+                        This site is a test project. Made by{' '}
+                        <a href="https://github.com/DaXBka" title="GitHub profile" target="_blank" rel="noreferrer">
+                            DaXBka
+                        </a>
+                    </span>
+                </FooterInner>
+            </Container>
+        </FooterOuter>
+    );
+};
+
+export default Footer;
