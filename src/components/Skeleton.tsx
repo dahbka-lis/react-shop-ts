742975db17ed0051ca076fcbd5ff83d3e4e4087f
DaHbka <flexinzflexinz@gmail.com>
2022-06-22 15:38:41 +0200

    Fix skeleton bug on mobile

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -5,6 +5,24 @@ import { ThemeContext } from '../context/ThemeContext';
 const Skeleton = () => {
     const theme = useContext(ThemeContext);
 
+    if (window.innerWidth <= 400) {
+        return (
+            <ContentLoader
+                speed={1.7}
+                width={315}
+                height={397}
+                viewBox="0 0 315 397"
+                backgroundColor={theme?.isDarkTheme ? '#6b6b6b' : '#f3f3f3'}
+                foregroundColor={theme?.isDarkTheme ? '#555555' : '#e4e4e4'}
+            >
+                <rect x="-2" y="1" rx="10" ry="10" width="315" height="200" />
+                <rect x="23" y="227" rx="10" ry="10" width="267" height="46" />
+                <rect x="23" y="315" rx="10" ry="10" width="61" height="26" />
+                <rect x="173" y="311" rx="20" ry="20" width="119" height="36" />
+            </ContentLoader>
+        );
+    }
+
     return (
         <ContentLoader
             speed={1.7}
