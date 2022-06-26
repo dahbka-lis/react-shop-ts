729a0257aca31af6533485cb4fcfb6273e789985
DaHbka <flexinzflexinz@gmail.com>
2022-06-26 19:40:29 +0200

    work with ProductItemPage

--- src/pages/ProductItem.tsx
+++ src/pages/ProductItem.tsx
@@ -1,12 +1,40 @@
-import { FC } from 'react';
+import axios from 'axios';
+import { FC, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { CardItem } from '../@types/card';
 import { Container, Main } from '../components/globalStyled';
 
 type IProductItemPageProps = {};
 
+const PageInner = styled.div``;
+
 const ProductItemPage: FC<IProductItemPageProps> = () => {
+    const { id } = useParams();
+    const [product, setProduct] = useState<CardItem | null>(null);
+
+    useEffect(() => {
+        let ignore = false;
+
+        const fetchProduct = async () => {
+            const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+            setProduct(response.data);
+        };
+
+        if (!ignore) fetchProduct();
+
+        return () => {
+            ignore = true;
+        };
+    }, []);
+
+    if (product === null) {
+        return <h1>Wait!</h1>;
+    }
+
     return (
         <Main>
-            <Container>ProductItemPage</Container>
+            <Container>{product.title}</Container>
         </Main>
     );
 };
